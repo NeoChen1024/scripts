d2574ae17119d49129284a50e6777a6a8e077f48
Neo_Chen <chenkolei@gmail.com>
2017-11-03 22:52:29 +0800

    Added git-autopull.sh

---
+++ git-autopull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Git-Autopull script -- update all repositorys under a directory
+# Usage : git-autopull.sh [dir]
+set -ev
+if [ -v $1 ]||[ $1 == "." ]; then
+	olddir="$PWD"
+	cd "$1"
+fi
+
+for i in */ ;do
+	cd "$i"
+	echo -e "\e[1;34m>>\e[32m$i\e[1;34m<<\e[0m"
+	git pull
+	echo -e "\e[36m========\n"
+	cd ..
+done
+echo "Done!"
+cd "$olddir"
+return 0
