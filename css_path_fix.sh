983eb5667443de43bfb20f2d2916b926d198c2e7
Neo_Chen <chenkolei@gmail.com>
2017-11-04 20:32:01 +0800

    d3c01853-7f8c-485d-b9bb-b6e2f6990b7c

--- css_path_fix.sh
+++ css_path_fix.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-
-FPATH="./www/"
+set -e
 TMPFILE="/tmp/sed-$UID.tmp"
+DIR="$HOME/www"
+printf "\e[s"
+all=$(find "$DIR" -name \*.html | wc -l)
+declare -i count=0
 
-for i in $(find "$FPATH" -name \*.html) ; do
-	if grep 'href="/neostyle.css"' "$i" ; then
-	sed 's|href="/neostyle.css"|href="/~neo_chen/neostyle.css"|' "$i" > "$TMPFILE"
-	cp -v "$TMPFILE" "$i"
+for i in $(find "$DIR" -name \*.html) ; do
+	count=$((++count))
+	printf "[$count/$all]\e[1;34m>>\e[32m$i\e[1;34m<<\e[0m"
+	if grep 'href="/neostyle.css"' "$i" > /dev/null 2>&1 ; then
+		sed 's|href="/neostyle.css"|href="/~neo_chen/neostyle.css"|' "$i" > "$TMPFILE"
+		printf "\n\t"
+		cp -v "$TMPFILE" "$i"
+		printf "\e[s"
+	else
+		printf "\e[2K\e[u"
 	fi
 done
+exit 0
