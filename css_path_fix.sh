ca277a405f9b51162d3fe3451cf08494c582db75
Neo_Chen <chenkolei@gmail.com>
2017-11-11 13:49:50 +0800

    Added Template.sh

--- css_path_fix.sh
+++ css_path_fix.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# A script for fixing my CSS Path Problem
+# Use with xargs
+
 set -e
-TMPFILE="/tmp/sed-$UID.tmp"
-DIR=${1:-$HOME/www}
+TMPFILE="/tmp/$RANDOM"
+DIR="$HOME/www"
+all="$#"
 printf "\e[s"
-all=$(find "$DIR" -name \*.html | wc -l)
 declare -i count=0
 
-for i in $(find "$DIR" -name \*.html) ; do
+for i in "$@" ; do
 	count=$((++count))
 	printf "[$count/$all]\e[1;34m>>\e[32m$i\e[1;34m<<\e[0m"
 	if grep 'href="/neostyle.css"' "$i" > /dev/null 2>&1 ; then
---
+++ template.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Bash Script Template
+SCRIPT_NAME="Template"
+AURTHOR="Name <email addr>"
+LICENSE="GPLv3 or Newer"
+
+# usage() is important
+usage()
+{
+	echo "Usage: $SCRIPT_NAME [-v] [-h] [-f filename]"
+}
+
+# Get command line aruguments
+
+while getopts “hf:v” OPTION ; do
+	case $OPTION in
+	h)
+		usage
+		exit 1
+		;;
+	f)
+		FILE=$OPTARG
+		;;
+	v)
+		VERBOSE=1
+		;;
+	esac
+done
+
+echo "This script does nothing."
