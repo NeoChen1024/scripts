05c73a75aa19fb56ae69532f5bbd9a20f4c0a691
Neo_Chen <chenkolei@gmail.com>
2017-11-27 17:43:09 +0800

    template.sh is prepared for color output now

--- template.sh
+++ template.sh
@@ -27,4 +27,29 @@ while getopts "hf:v" OPTION ; do
 	esac
 done
 
-echo "This script does nothing."
+## Colors
+ESC="\033"
+RESET="${ESC}[0m"	#Reset all attributes
+BRIGHT="${ESC}[1m"	#Bright
+DIM="${ESC}[2m"	#Dim
+BLINK="${ESC}[5m"	#Blink
+#Foreground Colours
+FBLACK="${ESC}[30m"	#Black
+FRED="${ESC}[31m"	#Red
+FGREEN="${ESC}[32m"	#Green
+FYELLOW="${ESC}[33m"	#Yellow
+FBLUE="${ESC}[34m"	#Blue
+FMAGENTA="${ESC}[35m"	#Magenta
+FCYAN="${ESC}[36m"	#Cyan
+FWHITE="${ESC}[37m"	#White
+#Background Colours
+BBLACK="${ESC}[40m"	#Black
+BRED="${ESC}[41m"	#Red
+BGREEN="${ESC}[42m"	#Green
+BYELLOW="${ESC}[43m"	#Yellow
+BBLUE="${ESC}[44m"	#Blue
+BMAGENTA="${ESC}[45m"	#Magenta
+BCYAN="${ESC}[46m"	#Cyan
+BWHITE="${ESC}[47m"	#White
+
+echo -e "${FGREEN}This ${FYELLOW}script ${FCYAN}does ${FMAGENTA}nothing.${RESET}"
