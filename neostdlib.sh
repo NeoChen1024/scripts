a4fb0697198d089e8d1a52b2edc353dc0a66bd72
Neo_Chen <chenkolei@gmail.com>
2017-11-29 22:25:23 +0800

    Added some useful feature into Standard Library

--- neostdlib.sh
+++ neostdlib.sh
@@ -63,7 +63,7 @@ BWHITE="${ESC}[47m"	#White
 # Normal Message
 msg_echo()
 {
-# Argument :: $1
+# Argument::	$1
 	if [ "$QUIET" -lt 1 ] ;then
 		echo -e "${BBLUE}>>${RESET} ${FCYAN}${1}${RESET}"
 	fi
@@ -72,7 +72,7 @@ msg_echo()
 # Debug Level Verbose
 verbose2_echo()
 {
-# Argument :: $1
+# Argument::	$1
 	if [ "$VERBOSE" -ge 2 ] ;then
 		echo -e "${BRIGHT}${BBLUE}>>${RESET} ${BRIGHT}${FGREEN}${1}${RESET}"
 	fi
@@ -81,31 +81,43 @@ verbose2_echo()
 # Verbose Message
 verbose_echo()
 {
-# Argument :: $1
+# Argument::	$message
 	if [ "$VERBOSE" -ge 1 ] ;then
 		echo -e "${BRIGHT}${BBLUE}>>${RESET} ${BRIGHT}${FCYAN}${1}${RESET}"
 	fi
 }
 
+# Check whether the variable is set or not
 is_set()
 {
-# Argument ::	$name $value
-# Return Err ::	When $value is not set or its length is zero
+# Argument::	$name
+# Return Err::	When $value is not set or its length is zero
 	local name
 	local value
 	name="$1"
-	value="$2"
 
-	if test -z "${value}" ;then
+	if [ -z "${!name}" ] ;then
 	msg_echo "${name} is not set"
 	return 2
 	fi
 }
 
+# Check whether the file is exist or not
+is_exist()
+{
+# Argument::	$file
+# Return Err::	When $file is not exist
+	local file
+	file="$1"
+
+	[ -f "$file" ] || return 2
+}
+
+# Expand variables from configuration
 expand_variables()
 {
-# Argument :: 	$prefix $class $count
-# Return Var ::	$prefix_class_name
+# Argument :: 	$<prefix>, $<class>, $count
+# Return Var ::	$<prefix>_<class>_<name>
 	local var_class_name
 	local var_class_item
 	local prefix
@@ -117,10 +129,40 @@ expand_variables()
 	count="$3"
 
 	var_class_name="${prefix}_${class}"
-	var_class_item="${prefix}_${class}_${#var_class_name[$count]}"
-	declare "${prefix}_${class}_${#var_class_name[$count]}=${#var_class_item}"
+	var_class_item="${prefix}_${class}_${!var_class_name[$count]}"
+	declare "${prefix}_${class}_${!var_class_name[$count]}=${!var_class_item}"
 
-	is_set "$var_class_name" "${#var_class_name}"
-	is_set "$var_class_item" "${#var_class_item}"
+	is_set "$var_class_name" "${!var_class_name}"
+	is_set "$var_class_item" "${!var_class_item}"
+}
+
+random()
+{
+# Argument :: $source (random|pseudo), $length [bytes]
+	local source
+	local length
+
+	source="$1"
+	length="$2"
+	
+	case "$source" in
+		random)	dd if=/dev/random of=/dev/stdout bs=1 count="$length" ;;
+		pseudo)	dd if=/dev/urandom of=/dev/stdout bs=1 count="$length" ;;
+	esac
+}
+
+hexify()
+{
+# Argument :: $source (stdio|file)
+	local source
+	local file
+	source="$1"
+	file="$2"
+
+	if [ "$source" == "stdio" ] ;then
+		od -t x2
+	elif [ "$source" == "file" ]&&[ -f "$file" ] ;then
+		od -t x2 "$file"
+	fi
 }
 
