509f0bc35545ee76bb704681734996603d23723e
Neo_Chen <chenkolei@gmail.com>
2023-01-26 12:22:46 +0800

    transcoding.sh: software encoding

--- transcoding.sh
+++ transcoding.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ffmpeg -init_hw_device qsv=hw -filter_hw_device hw \
+ffmpeg \
 	-i "$1" \
-	-c:a libopus -b:a 64k \
-	-vf 'hwupload=extra_hw_frames=64,vpp_qsv=denoise=100' -c:v hevc_qsv -preset veryslow -b:v 384k -maxrate 384k -g 60 -bufsize 2M \
+	-c:a libopus -b:a 256k \
+	-c:v hevc -q:v 23 -preset veryslow -b:v 2048k -maxrate 3072k -g 60 -bufsize 8M \
 	-c:s copy \
 	"${1%.*}-small.mkv"
