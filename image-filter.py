168fb4de70bbe9b00ddfe2037e034b8278e565f5
Neo_Chen <chenkolei@gmail.com>
2025-02-08 20:35:32 +0800

    image-filter.py: fix keyboard interrupt and batch_size

--- image-filter.py
+++ image-filter.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import multiprocessing as mp
-from multiprocessing import JoinableQueue, Queue, Process
+from multiprocessing import Queue, Process
 import os
-from pprint import pprint as pp
 from shutil import copy2, move
 
 import click
@@ -24,7 +23,7 @@ modes = {
 
 # Process Image
 def load_image(
-    file_path_queue: JoinableQueue,
+    file_path_queue: Queue,
     image_return_queue: Queue,
 ):
     print(f"Image Loader {os.getpid()} started")
@@ -39,8 +38,6 @@ def load_image(
             img.thumbnail((1024, 1024))
         # will block if the return queue is full
         image_return_queue.put((file_path, img))
-        # TODO: figure out whether this will cause deadlock or not
-        file_path_queue.task_done()
     print(f"Image Loader {os.getpid()} exiting")
 
 
@@ -106,93 +103,118 @@ def filter_images(
     name_sort=False,
     mode="copy",
 ):
-    accelerator = Accelerator()
-    actual_device = accelerator.device
-    if device is not None:
-        actual_device = device
-    pipe = pipeline(
-        "image-classification",
-        use_fast=True,
-        model=model,
-        device=actual_device,
-    )
-    if not noop:
-        # Create the output directories if they don't exist
-        os.makedirs(output_dir, exist_ok=True)
-        os.makedirs(rejected_dir, exist_ok=True)
-
-    if not (threshold >= 0 and threshold <= 1):
-        raise ValueError("Threshold must be between 0 and 1")
-
-    file_func = modes[mode]
-
-    file_path_queue = JoinableQueue()
-    image_return_queue = Queue(maxsize=batch_size * 2)
-    # Launch the image loading processes
-    num_processes = min(mp.cpu_count(), batch_size)
-    processes = []
-    for i in range(num_processes):
-        p = Process(
-            name=f"image_loader_{i}",
-            daemon=True,
-            target=load_image,
-            args=(file_path_queue, image_return_queue),
+    try:
+        accelerator = Accelerator()
+        actual_device = accelerator.device
+        if device is not None:
+            actual_device = device
+        pipe = pipeline(
+            "image-classification",
+            use_fast=True,
+            model=model,
+            device=actual_device,
+            batch_size=batch_size,
         )
-        p.start()
-        processes.append(p)
+        if not noop:
+            # Create the output directories if they don't exist
+            os.makedirs(output_dir, exist_ok=True)
+            os.makedirs(rejected_dir, exist_ok=True)
 
-    # Load all image file paths
-    for root, _, files in os.walk(input_dir):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if filetype.is_image(file_path):
-                file_path_queue.put(file_path)
+        if not (threshold >= 0 and threshold <= 1):
+            raise ValueError("Threshold must be between 0 and 1")
 
-    # Process images in batches
-    while not image_return_queue.empty() or not file_path_queue.empty():
-        batch = []
-        return_image_file_paths = []
-        print(f"Processing batch, available / batch size = {image_return_queue.qsize()} / {batch_size}")
-        while len(batch) < batch_size:
-            if not image_return_queue.empty():
-                (file_path, img) = image_return_queue.get()
-                batch.append(img)
-                return_image_file_paths.append(file_path)
-            elif not file_path_queue.empty():
-                continue
-            else:
-                break
-        print("Actual loaded batch size", len(batch))
+        file_func = modes[mode]
 
-        # Perform classification for the batch
-        results = pipe(images=batch)
+        file_path_queue = Queue()
+        image_return_queue = Queue(maxsize=batch_size * 2)
+        # Launch the image loading processes
+        num_processes = min(mp.cpu_count(), batch_size)
+        processes = []
+        for i in range(num_processes):
+            p = Process(
+                name=f"image_loader_{i}",
+                daemon=True,
+                target=load_image,
+                args=(file_path_queue, image_return_queue),
+            )
+            p.start()
+            processes.append(p)
 
-        for idx, result in enumerate(results):
-            bn = os.path.basename(return_image_file_paths[idx])
-            # Extract the prediction scores and labels
-            predictions = result
+        # Load all image file paths
+        for root, _, files in os.walk(input_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if filetype.is_image(file_path):
+                    file_path_queue.put(file_path)
 
-            # Determine the destination folder based on the prediction and threshold
-            hq_score = [p for p in predictions if p["label"] == "hq"][0]["score"]
-            destination_folder = output_dir if hq_score >= threshold else rejected_dir
+        # Process images in batches
+        while not image_return_queue.empty() or not file_path_queue.empty():
+            batch = []
+            return_image_file_paths = []
+            print(f"Processing batch, available / batch size = {image_return_queue.qsize()} / {batch_size}")
+            while len(batch) < batch_size:
+                if not image_return_queue.empty():
+                    (file_path, img) = image_return_queue.get()
+                    batch.append(img)
+                    return_image_file_paths.append(file_path)
+                elif not file_path_queue.empty():
+                    continue
+                else:
+                    break
+            print("Actual loaded batch size", len(batch))
 
-            print(f"[{hq_score:0.3f}] {bn}")
-            # Copy the image to the appropriate folder
-            if not noop:
-                if name_sort:
-                    bn = f"{hq_score * 1000:4.0f}_{bn}"
-                file_func(
-                    batch[idx],
-                    os.path.join(destination_folder, bn),
-                )
-                print(f"{mode} {batch[idx]} to {destination_folder}")
+            # Perform classification for the batch
+            results = pipe(images=batch)
 
-    # Send exit signal to the image loading processes
-    for _ in range(num_processes * 2):
-        file_path_queue.put(None)
-    for p in processes:
-        p.join()
-    print("All images processed")
+            for idx, result in enumerate(results):
+                bn = os.path.basename(return_image_file_paths[idx])
+                # Extract the prediction scores and labels
+                predictions = result
+
+                # Determine the destination folder based on the prediction and threshold
+                hq_score = [p for p in predictions if p["label"] == "hq"][0]["score"]
+                destination_folder = output_dir if hq_score >= threshold else rejected_dir
+
+                print(f"[{hq_score:0.3f}] {bn}")
+                # Copy the image to the appropriate folder
+                if not noop:
+                    if name_sort:
+                        bn = f"{hq_score * 1000:4.0f}_{bn}"
+                    file_func(
+                        batch[idx],
+                        os.path.join(destination_folder, bn),
+                    )
+                    print(f"{mode} {batch[idx]} to {destination_folder}")
+
+        # Send exit signal to the image loading processes
+        for _ in range(num_processes * 2):
+            file_path_queue.put(None)
+        for p in processes:
+            p.join()
+        print("All images processed")
+
+    except KeyboardInterrupt:
+        # TODO: find a better way to handle this
+        print("Caught KeyboardInterrupt, exiting")
+        # Drain the input queue
+        for _ in range(file_path_queue.qsize()):
+            try:
+                file_path_queue.get_nowait()
+            except mp.queues.Empty:
+                pass
+        # Put None to signal the image loading processes to exit
+        for _ in range(num_processes * 2): 
+            file_path_queue.put(None)
+        # Drain the return queue to ensure the image loading processes exit
+        for _ in range(image_return_queue.qsize()):
+            try:
+                image_return_queue.get_nowait()
+            except mp.queues.Empty:
+                pass
+        for p in processes:
+            p.terminate()
+        for p in processes:
+            p.join()
 
 
 if __name__ == "__main__":
