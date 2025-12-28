a79866c2bbb6850146721770da94bfdcfcd8c21e
Neo_Chen <chenkolei@gmail.com>
2025-12-28 15:27:15 +0800

    refactor(image_filter.py): streamline image processing logic and improve error handling

--- python-src/neoscripts/image_filter.py
+++ python-src/neoscripts/image_filter.py
@@ -124,97 +124,94 @@ def __main__(
     mode="copy",
     buffer_multiplier=2,
 ):
-    try:
-        accelerator = Accelerator()
-        actual_device = accelerator.device
-        if device is not None:
-            actual_device = device
-        model = ViTForImageClassification.from_pretrained(
-            model_name,
-            attn_implementation="sdpa",
-        )
-        processor = AutoImageProcessor.from_pretrained(model_name, use_fast=True)
-        pipe = pipeline(
-            "image-classification",
-            use_fast=True,
-            model=model,
-            image_processor=processor,
-            device=actual_device,
-            batch_size=batch_size,
-        )
-        if not noop:
-            # Create the output directories if they don't exist
-            os.makedirs(output_dir, exist_ok=True)
-            os.makedirs(rejected_dir, exist_ok=True)
+    accelerator = Accelerator()
+    actual_device = accelerator.device
+    if device is not None:
+        actual_device = device
+    model = ViTForImageClassification.from_pretrained(
+        model_name,
+        attn_implementation="sdpa",
+    )
+    processor = AutoImageProcessor.from_pretrained(model_name, use_fast=True)
+    pipe = pipeline(
+        "image-classification",
+        use_fast=True,
+        model=model,
+        image_processor=processor,
+        device=actual_device,
+        batch_size=batch_size,
+    )
+    if not noop:
+        # Create the output directories if they don't exist
+        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(rejected_dir, exist_ok=True)
 
-        if not (threshold >= 0 and threshold <= 1):
-            raise ValueError("Threshold must be between 0 and 1")
+    if not (threshold >= 0 and threshold <= 1):
+        raise ValueError("Threshold must be between 0 and 1")
 
-        file_func = modes[mode]
+    file_func = modes[mode]
 
-        num_processes = min(mp.cpu_count(), batch_size)
+    num_processes = min(mp.cpu_count(), batch_size)
 
-        image_file_paths = []
+    image_file_paths = []
 
-        # Load all image file paths
-        for root, _, files in os.walk(input_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                if filetype.is_image(file_path):
-                    image_file_paths.append(file_path)
+    # Load all image file paths
+    for root, _, files in os.walk(input_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if filetype.is_image(file_path):
+                image_file_paths.append(file_path)
 
-        data = torch.utils.data.DataLoader(
-            ImageDatasetLoader(image_file_paths),
-            batch_size=batch_size,
-            collate_fn=collate_fn,
-            num_workers=num_processes,
-            prefetch_factor=buffer_multiplier,
-        )
+    data = torch.utils.data.DataLoader(
+        ImageDatasetLoader(image_file_paths),
+        batch_size=batch_size,
+        collate_fn=collate_fn,
+        num_workers=num_processes,
+        prefetch_factor=buffer_multiplier,
+    )
 
-        # Process images in batches
-        t = tqdm(data, total=len(data), smoothing=0)
-        for batch in t:
-            batch_images, original_image_path = [], []
-            for imgs, paths in batch:
-                batch_images.append(imgs)
-                original_image_path.append(paths)
-            # TODO: find a way to silence this warning by pipeline:
-            # "You seem to be using the pipelines sequentially on GPU. In order to maximize efficiency please use a dataset"
-            # NOTE: It's still using DataLoader internally, there's no difference.
-            # Then there's no way to pass a DataLoader to the pipeline, nor a collate_fn.
-            # Letting it load data by itself is a bad idea too, as there can be errors in loading images, and
-            # we need a reliable way to preserve the input image paths.
-            result = pipe(batch_images)
-            if result is None:
-                t.write("No result from pipeline")
-                continue
-            for result, image_path in zip(result, original_image_path):
-                # Get the path of image relative to the input directory
-                relative_filepath = os.path.relpath(image_path, input_dir)
-                # separate the path and the filename
-                relative_dirpath = os.path.join(*relative_filepath.split(os.path.sep)[:-1])
-                bn = os.path.basename(image_path)
-                # Extract the prediction scores and labels
-                predictions = result
+    # Process images in batches
+    t = tqdm(data, total=len(data), smoothing=0)
+    for batch in t:
+        batch_images, original_image_path = [], []
+        for imgs, paths in batch:
+            batch_images.append(imgs)
+            original_image_path.append(paths)
+        # TODO: find a way to silence this warning by pipeline:
+        # "You seem to be using the pipelines sequentially on GPU. In order to maximize efficiency please use a dataset"
+        # NOTE: It's still using DataLoader internally, there's no difference.
+        # Then there's no way to pass a DataLoader to the pipeline, nor a collate_fn.
+        # Letting it load data by itself is a bad idea too, as there can be errors in loading images, and
+        # we need a reliable way to preserve the input image paths.
+        result = pipe(batch_images)
+        if result is None:
+            t.write("No result from pipeline")
+            continue
+        for result, image_path in zip(result, original_image_path):
+            # Get the path of image relative to the input directory
+            relative_filepath = os.path.relpath(image_path, input_dir)
+            # separate the path and the filename
+            relative_dirpath = os.path.dirname(relative_filepath)
+            bn = os.path.basename(image_path)
+            # Extract the prediction scores and labels
+            predictions = result
 
-                # Determine the destination dir based on the prediction and threshold
-                hq_score = [p for p in predictions if p["label"] == "hq"][0]["score"]
-                destination_dir = output_dir if hq_score >= threshold else rejected_dir
-                destination_dir = os.path.join(destination_dir, relative_dirpath)
+            # Determine the destination dir based on the prediction and threshold
+            hq_score = [p for p in predictions if p["label"] == "hq"][0]["score"]
+            destination_dir = output_dir if hq_score >= threshold else rejected_dir
+            destination_dir = os.path.join(destination_dir, relative_dirpath)
 
-                t.write(f"[{hq_score:0.3f}] {bn}")
-                if not noop:
-                    if name_sort:
-                        bn = f"{hq_score * 1000:04.0f}_{bn}"
-                    os.makedirs(destination_dir, exist_ok=True)
-                    file_func(
-                        image_path,
-                        os.path.join(destination_dir, bn),
-                    )
-                    t.write(f"{mode} {image_path} -> {destination_dir}")
-        return
-    except Exception as e:
-        print(f"Error: {e}")
+            t.write(f"[{hq_score:0.3f}] {bn}")
+            if not noop:
+                if name_sort:
+                    bn = f"{hq_score * 1000:04.0f}_{bn}"
+                os.makedirs(destination_dir, exist_ok=True)
+                file_func(
+                    image_path,
+                    os.path.join(destination_dir, bn),
+                )
+                t.write(f"{mode} {image_path} -> {destination_dir}")
+    return
 
 
 if __name__ == "__main__":
